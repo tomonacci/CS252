248194c5f77b3092ef72c4dd8dfab28f33694fe5
Tomonaru Kazahaya <tomonacci@gmail.com>
2015-10-21 06:29:46 -0400

    Redefine concrete tCESK*

--- aam.rkt
+++ aam.rkt
@@ -81,60 +81,45 @@
 
 ; e.g.: (traces red-CESK* (inj-CESK* '((λ f (f f)) (λ x x))))
 
-(define-language t-CESK*
-  (x variable-not-otherwise-mentioned)
-  (v (lmd x e)
-     number)
-  (op * / + -)
+(define-extended-language tCESK* LC
   (a number)
   (t number)
-  (e x
-     v
-     (e e)
-     (op e ...))
-  (vp (v p))
-  (c (e p))
-  (p ((x a) ...))
-  (k mt
-     (arg c a)
-     (fun vp a)
-     (op (vp ...) (c ...) a))
-  (z vp k)
-  (s ((a z) ...)))
-(define-metafunction t-CESK*
-  [(inj e) ((e ()) ((0 mt)) 0 1)])
-(define-metafunction t-CESK*
-  [(dlt * number ...) ,(apply * (term (number ...)))]
-  [(dlt / number ...) ,(apply / (term (number ...)))]
-  [(dlt + number ...) ,(apply + (term (number ...)))]
-  [(dlt - number ...) ,(apply - (term (number ...)))])
-(define-metafunction t-CESK*
-  [(lookup x p) ,(second (findf (lambda (obj) (equal? (first obj) (term x))) (term p)))]
-  [(lookup a s) ,(second (findf (lambda (obj) (equal? (first obj) (term a))) (term s)))])
-(define-metafunction t-CESK*
-  [(insert x a p) ,(cons (term (x a)) (term p))]
-  [(insert a z s) ,(cons (term (a z)) (term s))])
-(define-metafunction t-CESK*
-  [(tick t) ,(+ (term t) 1)])
-(define-metafunction t-CESK*
-  [(alloc t) t])
-(define reduce
+  (rho any)
+  (kappa mt (arg c a) (fun vp a))
+  (sigma any))
+
+; There actually is a minor mistake in the paper, claiming that they can have t_0 = 0.
+; This is wrong since it will lead to overwriting (0 . mt) with some other value.
+; Here we choose t_0 = 1.
+(define (inj-tCESK* expr)
+  (term (,expr () ((0 . mt)) 0 1)))
+
+(define (tick t) (+ t 1))
+(define alloc identity)
+
+(define red-tCESK*
   (reduction-relation
-   t-CESK*
-   (--> ((x p) s a t) ((lookup (lookup x p) s) s a (tick t)))
-   (--> (((e_1 e_2) p) s a t) ((e_1 p) (insert (alloc t) (arg (e_2 p) a) s) (alloc t) (tick t)))
-   (--> (vp ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (arg c a_4)) (a_2 z_2) ...))])
-           (term (c (insert (alloc t) (fun vp a_4) ,s) (alloc t) (tick t)))))
-   (--> (vp ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (fun ((lmd x e) p) a_4)) (a_2 z_2) ...))])
-           (term ((e (insert x (alloc t) p)) (insert (alloc t) vp ,s) a_4 (tick t)))))
-   (--> (((op e_1 e_2 ...) p) s a t)
-        ,(let ([cs (map (lambda (e) (list e (term p))) (term (e_2 ...)))])
-           (term ((e_1 p) (insert (alloc t) (op () ,cs a) s) (alloc t) (tick t)))))
-   (--> (vp_1 ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (op (vp_2 ...) (c_1 c_2 ...) a_4)) (a_2 z_2) ...))])
-           (term (c_1 (insert (alloc t) (op (vp_2 ... vp_1) (c_2 ...) a_4) ,s) (alloc t) (tick t)))))
-   (--> ((v_1 p_1) ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...) a_3 t)
-        ,(let ([s (term ((a_1 z_1) ... (a_3 (op ((v_2 p_2) ...) () a_4)) (a_2 z_2) ...))])
-           (term (((dlt op v_2 ... v_1) ()) ,s a_4 (tick t)))))))
+   tCESK*
+   (--> (x rho sigma a t)
+        ,(receive (v rho) (car+cdr (dict-ref (term sigma) (dict-ref (term rho) (term x))))
+           (term (,v ,rho sigma a ,(tick (term t)))))
+        "#1")
+   (--> ((e_0 e_1) rho sigma a t)
+        ,(let ([b (alloc (term t))])
+           (term (e_0 rho ,(dict-set (term sigma) b (term (ar e_1 rho a))) ,b ,(tick (term t)))))
+        "#2")
+   (--> (v rho_0 sigma a_0 t)
+        ,(let ([b (alloc (term t))])
+           (term (e rho_1 ,(dict-set (term sigma) b (term (fn v rho_0 a_1))) ,b ,(tick (term t)))))
+        (where (ar e rho_1 a_1) ,(dict-ref (term sigma) (term a_0)))
+        "#3")
+   (--> (v rho_0 sigma a_0 t)
+        ,(let ([b (alloc (term t))])
+           (term (e
+                  ,(dict-set (term rho_1) (term x) b)
+                  ,(dict-set (term sigma) b (cons (term v) (term rho_0)))
+                  a_1
+                  ,(tick (term t)))))
+        (where (fn (λ x e) rho_1 a_1) ,(dict-ref (term sigma) (term a_0)))
+        "#4")
+   ))
